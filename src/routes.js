77db5e3f46b684836f23ec5d0b369a41f5aa9c11
Caio Queiroz <caiogqueiroz@gmail.com>
2019-11-09 18:59:25 -0300

    feat: config e criacao das models plano e enrollment

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
+import PlanController from './app/controllers/PlanController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -14,5 +15,10 @@ routes.use(authMiddleware);
 
 routes.post('/students', StudentController.store);
 routes.put('/students/:id', StudentController.update);
+
+routes.post('/plans', PlanController.store);
+routes.put('/plans/:id', PlanController.update);
+routes.get('/plans', PlanController.index);
+routes.delete('/plans/:id', PlanController.delete);
 
 export default routes;
