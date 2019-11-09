d1b377c183c732d4bd418e8c933e3a2990b18dbb
Caio Queiroz <caiogqueiroz@gmail.com>
2019-11-09 18:57:58 -0300

    feat: crud planos

---
+++ src/app/controllers/PlanController.js
@@ -0,0 +1,55 @@
+import * as Yup from 'yup';
+
+import Plan from '../models/Plan';
+
+class PlanController {
+  async index(req, res) {
+    const plans = await Plan.findAll({});
+
+    return res.status(200).json(plans);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    console.log(req.body);
+
+    const plan = await Plan.create(req.body);
+    return res.status(201).json(plan);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      duration: Yup.number(),
+      price: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    let plan = await Plan.findByPk(req.params.id);
+    console.log(plan);
+
+    plan = await plan.update(req.body);
+
+    return res.status(200).json(plan);
+  }
+
+  async delete(req, res) {
+    const plan = await Plan.destroy({ where: { id: req.params.id } });
+
+    return res.json(plan);
+  }
+}
+
+export default new PlanController();
